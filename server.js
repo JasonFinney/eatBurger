e3093fbd48d549b3e3b52a9005450bedbc94cd16
Jason Finney <finney.jason.218@gmail.com>
2018-09-17 19:56:14 -0500

    working

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var burger = require("./models/burger");
-var mysql = require("mysql");
-var orm = require("./config/orm.js");
 var exphbs = require("express-handlebars");
 var app = express();
 var PORT = process.env.PORT || 9000;
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+var routes = require("./controllers/burgers_controller");
 app.use(routes);
-
-orm.selectAll();
-
-orm.insertOne();
-
-orm.updateOne();
 
 app.listen(PORT, function () {
     console.log("Server listening on: http://localhost:" + PORT);
